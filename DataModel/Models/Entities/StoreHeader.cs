66a1c244bf4848c25b28840d81abb2b253396925
binhussen <mhdhsna@gmail.com>
2022-11-02 16:46:25 +0300

    Update store entity

--- DataModel/Models/Entities/StoreHeader.cs
+++ DataModel/Models/Entities/StoreHeader.cs
@@ -6,8 +6,8 @@ namespace DataModel.Models.Entities
     {
         [Column("storeHeaderId")]
         public int id { get; set; }
-        public string itemNoInExpenditureRegister { get; set; }
-        public string noOfEntryInTheRegisterOfIncomingGoods { get; set; }
+        public string? itemNoInExpenditureRegister { get; set; }
+        public string? noOfEntryInTheRegisterOfIncomingGoods { get; set; }
         public string donor { get; set; }
         [ForeignKey(nameof(NotifyHeader))]
         public int notifyHeaderId { get; set; }
--- DataModel/Models/Entities/StoreItem.cs
+++ DataModel/Models/Entities/StoreItem.cs
@@ -8,11 +8,13 @@ namespace DataModel.Models.Entities
         public int id { get; set; }
         public string itemDescription { get; set; }
         public string model { get; set; }
-        public string serialNo { get; set; }
+        public string? serialNo { get; set; }
         public string type { get; set; }
         public string storeNo { get; set; }
         public string shelfNo { get; set; }
-        public int quantity { get; set; }
+        public int quantity { get; set; } = 1;
+        public int unitPrice { get; set; }
+        public int? totalPrice { get; set; }
         public int availableQuantity { get; set; }
         public int approvedQuantity { get; set; } = 0;
         public bool availability { get; set; } = true;
